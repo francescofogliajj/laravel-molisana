ecc0dd65f87d5937b92adbe07ebd89119ebc090c
francescofogliajj <francescofogliajj@gmail.com>
2021-02-03 19:08:47 +0100

    Commit #2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,76 +1,47 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>La Molisana</title>
-
-        <!-- css -->
-        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
-        
-    </head>
-    <body>
-
-        <header>
-            <img src="{{ asset("img/marchio-sito-test.png") }}" alt="la-molisana">
-            <ul class="list-inline">
-                <li class="active">
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="#">Prodotti</a>
-                </li>
-                <li>
-                    <a href="#">News</a>
-                </li>
-            </ul>
-        </header>
-
-        <main>
-            <section class="container">
-                <h2>Le Lunghe</h2>
-                <div class="cards">
-                    @foreach ($lunghe as $pasta)
-                        <div class="card">
-                            <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
-                            <div class="card-text">
-                                <a href="#">{{ $pasta["titolo"] }}</a>
-                            </div>
+@section('content')
+    <main>
+        <section class="container">
+            <h2>Le Lunghe</h2>
+            <div class="cards">
+                @foreach ($lunghe as $pasta)
+                    <div class="card">
+                        <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+                        <div class="card-text">
+                            <a href="#">{{ $pasta["titolo"] }}</a>
                         </div>
-                    @endforeach
-                </div>
-            </section>
+                    </div>
+                @endforeach
+            </div>
+        </section>
 
-            <section class="container">
-                <h2>Le Corte</h2>
-                <div class="cards">
-                    @foreach ($corte as $pasta)
-                        <div class="card">
-                            <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
-                            <div class="card-text">
-                                <a href="#">{{ $pasta["titolo"] }}</a>
-                            </div>
+        <section class="container">
+            <h2>Le Corte</h2>
+            <div class="cards">
+                @foreach ($corte as $pasta)
+                    <div class="card">
+                        <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+                        <div class="card-text">
+                            <a href="#">{{ $pasta["titolo"] }}</a>
                         </div>
-                    @endforeach
-                </div>
-            </section>
+                    </div>
+                @endforeach
+            </div>
+        </section>
 
-            <section class="container">
-                <h2>Le Cortissime</h2>
-                <div class="cards">
-                    @foreach ($cortissime as $pasta)
-                        <div class="card">
-                            <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
-                            <div class="card-text">
-                                <a href="#">{{ $pasta["titolo"] }}</a>
-                            </div>
+        <section class="container">
+            <h2>Le Cortissime</h2>
+            <div class="cards">
+                @foreach ($cortissime as $pasta)
+                    <div class="card">
+                        <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+                        <div class="card-text">
+                            <a href="#">{{ $pasta["titolo"] }}</a>
                         </div>
-                    @endforeach
-                </div>
-            </section>
-        </main>
-
-        <footer></footer>
-    </body>
-</html>
+                    </div>
+                @endforeach
+            </div>
+        </section>
+    </main>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <img src="{{ asset("img/marchio-sito-test.png") }}" alt="la-molisana">
+    <ul class="list-inline">
+        <li class="active">
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Prodotti</a>
+        </li>
+        <li>
+            <a href="#">News</a>
+        </li>
+    </ul>
+</header>
